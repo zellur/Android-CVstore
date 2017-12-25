0054b23d93fafcf2fe42caf92714c241f0ec566f
rakib <zellurakib1234@gmail.com>
2017-12-26 05:25:47 +0600

    CV info page updated

--- app/src/main/java/com/hfad/cvstore/CreateCv.java
+++ app/src/main/java/com/hfad/cvstore/CreateCv.java
@@ -1,7 +1,10 @@
 package com.hfad.cvstore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CreateCv extends AppCompatActivity {
@@ -11,6 +14,16 @@ public class CreateCv extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_cv);
 
+        Button create = findViewById(R.id.create);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateCv.this,CvInfo.class);
+                startActivity(intent);
+
+            }
+        });
 
 
     }
